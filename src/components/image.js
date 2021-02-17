df991a32187747dc91df6d9d0ddf26fc6bd42df4
ClintFix <clint.fix@gmail.com>
2021-02-17 16:12:17 -0700

    added conditional logic to display videos

--- src/components/image.js
+++ src/components/image.js
@@ -3,22 +3,28 @@ import Axios from 'axios'
 
 
 export default function Image(props) {
-    const date = props;
+    const { date, setDate } = props;
     const [imgData, setImgData] = useState([]); //set image data to empty array
     //import img data on load
     useEffect(() => {
-        Axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date.date}`)
+        Axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`)
             .then(res => {
                 setImgData(res.data)
+                debugger
             })
-            .catch(err => console.log('There is an error with the Axios get'))
+            .catch(err => {
+                console.log('There is an error with the Axios get')
+                debugger
+            })
     }, [date]) //only render when date changes
 
     return (
         <div className='imgContainer'>
             <h2>{imgData.title}</h2>
             <div>
-                <img src = {imgData.url}></img>
+                {
+                    imgData.media_type === 'video' ? <iframe width="420" height="315" src={imgData.url}></iframe> : <img src = {imgData.url}></img> 
+                }
             </div>
             <div className='dateCopyright'>
                 <p>{imgData.date} · ©{imgData.copyright}</p>
